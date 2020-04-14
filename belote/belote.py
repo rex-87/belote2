0cba328c15686b12e701735db6a10fbff9337d4e
Romain Thibault <rex87@orange.fr>
2020-04-14 23:53:30 +0100

    GUI (WIP)

--- belote/belote.py
+++ belote/belote.py
@@ -59,7 +59,7 @@ try:
 			"Trefle" : "♣",
 		}
 		
-		value_l = [
+		rank_l = [
 			"7",
 			"8",
 			"9",
@@ -70,27 +70,27 @@ try:
 			"A",
 		]
 	
-		def __init__(self, suit, value):
+		def __init__(self, suit, rank):
 		
 			if suit not in self.suit_d.keys():
 				return Exception("Invalid suit {}".format(suit))
 
-			if value not in self.value_l:
-				return Exception("Invalid value {}".format(value))
+			if rank not in self.rank_l:
+				return Exception("Invalid rank {}".format(rank))
 
 			self.suit = suit
-			self.value = value
+			self.rank = rank
 			
 		def __repr__(self):
 		
-			return "{__class__.__name__}(suit={suit!r}, value={value!r})".format(
+			return "{__class__.__name__}(suit={suit!r}, rank={rank!r})".format(
 				__class__ = self.__class__,
 				**self.__dict__,
 			)
 			
 		def __str__(self):
 			
-			return "{:>2}".format(self.value)+self.suit_d[self.suit]
+			return "{:>2}".format(self.rank)+self.suit_d[self.suit]
 			
 		def get_rank(self, atout):
 		
@@ -105,7 +105,7 @@ try:
 					"A"  : 5,
 					"9"  : 6,
 					"V"  : 7,
-				}[self.value]
+				}[self.rank]
 				
 			else:
 
@@ -118,7 +118,7 @@ try:
 					"D"  : 5,
 					"R"  : 6,
 					"A"  : 7,
-				}[self.value]			
+				}[self.rank]			
 
 		def get_points(self, atout):
 		
@@ -133,7 +133,7 @@ try:
 					"A"  : 11,
 					"9"  : 14,
 					"V"  : 20,
-				}[self.value]
+				}[self.rank]
 				
 			else:
 
@@ -146,7 +146,7 @@ try:
 					"D"  : 3,
 					"R"  : 4,
 					"A"  : 11,
-				}[self.value]					
+				}[self.rank]					
 
 	class Deck(list):
 		
@@ -173,6 +173,9 @@ try:
 		
 			self.name = name
 			self.hand_l = Deck([])
+			self.b_human = False
+			self.b_thinking = False
+			self.human_choice = None
 			
 		def __repr__(self):
 			
@@ -185,20 +188,53 @@ try:
 			
 			return self.name
 	
-		def play_a_card(self, table_l, atout, regles):
+		def play_a_card(self, table_l, atout, regles, b_play_thread_abort_queue):
 			
 			# determine what cards can be played (according to the rules)
-			card_index_l = []
+			allowed_card_index_l = []
 			for c_index, c in enumerate(self.hand_l):
 				rest_of_hand = copy.copy(self.hand_l)
 				rest_of_hand.pop(c_index)
 				b_card_playable, card_playable_explanation = regles.b_is_card_playable(c, rest_of_hand, table_l, atout)
 				LOG.debug("{} {} {}".format(c, b_card_playable, "({})".format(card_playable_explanation)))
 				if b_card_playable:
-					card_index_l.append(c_index)
+					allowed_card_index_l.append(c_index)
 			
-			# current strategy: random
-			played_card = self.hand_l.pop(random.choice(card_index_l))
+			# loop to filter out wrong clicks on GUI
+			while True:
+			
+				if not self.b_human:
+					# current strategy: random
+					card_choice = random.choice(allowed_card_index_l)
+				else:
+					self.b_thinking = True
+					LOG.debug("Waiting for human player {}".format(self.name))
+					while True:
+
+						try:
+							b_play_thread_abort = b_play_thread_abort_queue.get(block = False)
+							if b_play_thread_abort:
+								raise Exception("Aborted while choosing a card")
+						except queue.Empty:
+							pass
+					
+						if self.human_choice is not None:
+							break
+							
+						time.sleep(1/20)
+						
+					card_choice = self.human_choice
+					self.human_choice = None				
+					self.b_thinking = False
+				
+				try:
+					played_card = self.hand_l.pop(card_choice)
+				except IndexError:
+					LOG.debug("IndexError while popping card from player hand. Likely problem with card clicked.")
+					continue
+				
+				break
+			
 			LOG.debug("{:<5}: {}".format(str(self), played_card))
 			
 			return played_card
@@ -334,7 +370,7 @@ try:
 		def get_winning_card_from_table(self, table_l, atout):
 		
 			"""
-			determine the winning card on the table 
+			TODO determine the winning card on the table 
 			"""
 	
 			couleur_demandee = self.get_couleur_demandee(table_l)
@@ -367,9 +403,11 @@ try:
 		
 			self.deck_l	   = None
 			self.joueurs_l = None
+			self.table_l   = None
+			self.winning_player = None
 			
-			self.test_thread = None
-			self.b_test_thread_abort_queue = queue.Queue()
+			self.play_thread = None
+			self.b_play_thread_abort_queue = queue.Queue()
 			
 			self.t = 0
 		
@@ -378,9 +416,10 @@ try:
 			self.deck_l	   = Deck(CsvObjectGenerator(Carte).generate_from_csv(csv_path = r'C:\Users\rex87\belote2\belote\cartes.csv' ))
 			self.joueurs_l = CsvObjectGenerator(Joueur).generate_from_csv(csv_path = r'C:\Users\rex87\belote2\belote\joueurs.csv')
 			
-			self.test_thread = threading.Thread(target = self.run_test_thread)		
+			# self.test_thread = threading.Thread(target = self.run_test_thread)		
+			self.play_thread = threading.Thread(name = "play_thread", target = self.run_play_thread)		
 		
-		def play(self):
+		def run_play_thread(self):
 			
 			# shuffle the deck
 			random.shuffle(self.deck_l)
@@ -393,11 +432,11 @@ try:
 			atout = "Coeur"
 
 			# game
-			table_l   = Deck([])
+			self.table_l = Deck([])
 			team1_l   = Deck([])
 			team2_l   = Deck([])
 			points_sum = 0
-			winning_player = 0
+			self.winning_player = 0
 			for round_i in range(8):
 
 				for j in self.joueurs_l:
@@ -406,32 +445,38 @@ try:
 				# table_l is indexed with table_i
 				for table_i in range(4):
 					
-					j_index = (table_i+winning_player)%4
+					j_index = (table_i+self.winning_player)%4
 					j = self.joueurs_l[j_index]
 					
 					# player plays a card
-					played_card = j.play_a_card(table_l, atout, Regles())
-					table_l.append(played_card)
+					played_card = j.play_a_card(self.table_l, atout, Regles(), self.b_play_thread_abort_queue)
+					self.table_l.append(played_card)
+					
+					time.sleep(0.5)
 				
-				winning_c_index = Regles().get_winning_card_from_table(table_l, atout)
-				winning_player = (winning_player+winning_c_index)%4
+				winning_c_index = Regles().get_winning_card_from_table(self.table_l, atout)
+				next_winning_player = (self.winning_player+winning_c_index)%4
 				
 				LOG.debug(
-					"Fin du tour: {}| Valeur de la main: {}| Joueur qui prend la main: {}".format(
-						table_l,
-						table_l.get_points(atout),
-						str(self.joueurs_l[winning_player]),
+					"Fin du tour: {}| Valeur de la main: {}| Joueur qui prend la main: {} ({})".format(
+						self.table_l,
+						self.table_l.get_points(atout),
+						str(self.joueurs_l[next_winning_player]),
+						next_winning_player,
 					)
 				)
+				input("PAUSE")
+				
+				self.winning_player = next_winning_player
 				
 				# end of round: empty table and in winning team's pile
-				if winning_player%2 == 0:
+				if self.winning_player%2 == 0:
 					winning_team_l = team1_l
 				else:
 					winning_team_l = team2_l
 				
 				for table_i in range(4):
-					winning_team_l.append(table_l.pop())
+					winning_team_l.append(self.table_l.pop())
 					
 				if round_i == 7:
 					winning_team_l.b_dix_de_der = True
@@ -472,17 +517,26 @@ try:
 					time.sleep(1/ClockFrequency - TimeElapsed) 		
 	
 		def start_all_threads(self):
-			self.test_thread.start()
+			# self.test_thread.start()
+			self.play_thread.start()
+			return
 		
 		def abortAllThreads(self):
-			self.b_test_thread_abort_queue.put(True)
+			# self.b_test_thread_abort_queue.put(True)
+			self.b_play_thread_abort_queue.put(True)
+			return
 		
 		def joinAllThreads(self):
-			self.test_thread.join()
+			# self.test_thread.join()
+			self.play_thread.join()
+			return
 		
 		def stopAllThreads(self):
 			self.abortAllThreads()
 			self.joinAllThreads()
+	
+	def get_hand_x(i):
+		return int(i*(screen_width-168)/7)
 	
 	this_folder = os.path.dirname(os.path.realpath(__file__))
 	
@@ -494,6 +548,10 @@ try:
 	WHITE_COLOUR = (255, 255, 255)
 
 	pygame.init()
+	pygame.font.init()
+	
+	myfont = pygame.font.SysFont('Consolas', 30)
+	
 	screen = pygame.display.set_mode((screen_width, screen_height))
 
 	background = pygame.Surface(screen.get_size())
@@ -501,10 +559,24 @@ try:
 
 	FPS = 30
 
-	belote_obj = Belote()
-	belote_obj.initialise()
-	belote_obj.start_all_threads()
+	hj = 2 # human joueur id
+	b_obj = Belote()
+	b_obj.initialise()
+	b_obj.start_all_threads()
+	b_obj.joueurs_l[hj].b_human = True
 
+	# table cards coordinates
+	tx = 320
+	ty = 120
+	to1, to2 = 70, 25
+	table_coord_l = [
+		(tx - to2, ty + to1), # human player
+		(tx - to1, ty - to2),
+		(tx + to2, ty - to1),
+		(tx + to1, ty + to2),
+	]
+
+	# load card images
 	card_images_d = {}
 	name_map = {
 		'pique'   : 'Pique',
@@ -525,42 +597,69 @@ try:
 		for rank in ['7', '8', '9', '10', 'valet', 'dame', 'roi', 'as']:
 			card_images_d[name_map[suit]][name_map[rank]] = pygame.image.load(os.path.join(this_folder, r'..\images\cards\{}_{}.png'.format(suit, rank)))
 
-	def get_card_x(i):
-		return int(i*(screen_width-168)/7)
-
 	b_playing = True
 	last_time = 0
-	card_y_idle = screen_height - 200
-	card_y_sel_offset = -50
+	hand_y_idle = screen_height - 120
+	hand_y_sel_offset = -50
 	while b_playing:
-
+	
+		# pygame events
+		b_left_click = False
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				b_playing = False # pygame window closed by user
-				belote_obj.stopAllThreads()
-			if event.type == pygame.MOUSEMOTION:
+				b_obj.stopAllThreads()
+			elif event.type == pygame.MOUSEMOTION:
 				mouse_pos = pygame.mouse.get_pos()
+			elif event.type == pygame.MOUSEBUTTONDOWN:
+				b_left_click = True
+				# LOG.info(mouse_pos)
 		
 		# the nice green background
 		background.fill(GREEN_COLOUR)
-
+		
 		# update card selection
 		b_hand_card_selected_l = [False]*8
-		if mouse_pos[1] > card_y_idle + card_y_sel_offset: 
+		if mouse_pos[1] > hand_y_idle + hand_y_sel_offset: 
+			
 			for hand_i in range(8):
 				
-				if mouse_pos[0] < get_card_x(hand_i+1):
+				if mouse_pos[0] >= get_hand_x(7+1):
+					b_hand_card_selected_l[7] = True
+					
+				if mouse_pos[0] < get_hand_x(hand_i+1):
 					b_hand_card_selected_l[hand_i] = True
 					break
-					
-			if mouse_pos[0] >= get_card_x(7+1):
-				b_hand_card_selected_l[7] = True
+			
+			if b_left_click:
+				b_obj.joueurs_l[hj].human_choice = hand_i
 		
+		# update table
+		for table_i, table_card in enumerate(b_obj.table_l):
+			
+			table_coord_i = (table_i - hj + b_obj.winning_player)%4
+			
+			table_x = table_coord_l[table_coord_i][0]
+			table_y = table_coord_l[table_coord_i][1]
+			background.blit(card_images_d[table_card.suit][table_card.rank], (table_x, table_y))
+			
 		# update card sprites
-		for hand_i in range(8):
-			card_x = get_card_x(hand_i)
-			card_y = card_y_idle + card_y_sel_offset*b_hand_card_selected_l[hand_i]
-			background.blit(card_images_d['Pique']['7'], (card_x, card_y))
+		for hand_i, hand_card in enumerate(b_obj.joueurs_l[hj].hand_l):
+			
+			hand_x = get_hand_x(hand_i)
+			hand_y = hand_y_idle + hand_y_sel_offset*b_hand_card_selected_l[hand_i]
+			
+			background.blit(card_images_d[hand_card.suit][hand_card.rank], (hand_x, hand_y))
+		
+		# display players' names
+		joueur_name  = myfont.render(b_obj.joueurs_l[(hj+0)%4].name, True, (0, 0, 0))
+		joueur_left  = myfont.render(b_obj.joueurs_l[(hj+1)%4].name, True, (0, 0, 0))
+		joueur_front = myfont.render(b_obj.joueurs_l[(hj+2)%4].name, True, (0, 0, 0))
+		joueur_right = myfont.render(b_obj.joueurs_l[(hj+3)%4].name, True, (0, 0, 0))
+		background.blit(joueur_name , (int(screen_width/2), screen_height-50      ))
+		background.blit(joueur_left , (0                  , int(4*screen_height/9)))
+		background.blit(joueur_front, (int(screen_width/2), 0                     ))
+		background.blit(joueur_right, (screen_width - 100 , int(5*screen_height/9)))
 		
 		# refresh rate
 		now = time.time()
@@ -581,4 +680,5 @@ except:
 ## -------- GIVE THE USER A CHANCE TO READ MESSAGES-----------
 finally:
 	
-	input("Press any key to exit ...")
+	pygame.quit()
+	# input("Press any key to exit ...")
